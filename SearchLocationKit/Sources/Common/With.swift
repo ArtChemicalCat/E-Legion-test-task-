ddb7050afbe6a486c846efff2b573d4cdde28f5c
Nikolay Kazanin <artchemist@yandex.ru>
2023-03-23 19:20:17 +0300

    rename package

---
+++ SearchLocationKit/Sources/Common/With.swift
@@ -0,0 +1,22 @@
+import class Foundation.NSObject
+
+public protocol WithObject {}
+public protocol WithValue {}
+
+public extension WithObject {
+    @discardableResult
+    func with(_ block: (Self) -> Void) -> Self {
+        block(self)
+        return self
+    }
+}
+
+public extension WithValue {
+    func with(_ block: (inout Self) -> Void) -> Self {
+        var mutated = self
+        block(&mutated)
+        return mutated
+    }
+}
+
+extension NSObject: WithObject { }
