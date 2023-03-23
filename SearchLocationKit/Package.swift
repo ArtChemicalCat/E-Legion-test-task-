97daa39ecf0d3b1361b40e9151b8e567d7d25595
Nikolay Kazanin <artchemist@yandex.ru>
2023-03-23 19:25:43 +0300

    models target

--- SearchLocationKit/Package.swift
+++ SearchLocationKit/Package.swift
@@ -10,6 +10,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Common", targets: ["Common"]),
+        .library(
+            name: "Models", targets: ["Models"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +22,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Common",
+            dependencies: []),
+        .target(
+            name: "Models",
             dependencies: []),
     ]
 )
